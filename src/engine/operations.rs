f08196993f53e4b2bb1a8146b8c07b2096abd241
Pranav M <pranavmofficial2005@gmail.com>
2026-04-10 09:24:28 +0530

    feat: define memory operation types and execution traits

---
+++ src/engine/operations.rs
@@ -0,0 +1,28 @@
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Operation {
+    MemCopy,
+    MemVecAdd,
+    MemVecAnd,
+    MemVecOr,
+    MemScan,
+    MemStrideScan,
+}
+
+impl Operation {
+    pub fn name(&self) -> &'static str {
+        match self {
+            Operation::MemCopy => "MEM_COPY",
+            Operation::MemVecAdd => "MEM_VEC_ADD",
+            Operation::MemVecAnd => "MEM_VEC_AND",
+            Operation::MemVecOr => "MEM_VEC_OR",
+            Operation::MemScan => "MEM_SCAN",
+            Operation::MemStrideScan => "MEM_STRIDE_SCAN",
+        }
+    }
+}
+
+pub trait MemoryOp {
+    fn execute_cpu(&self, buffers: &[&[u32]]) -> Vec<u32>;
+    fn execute_memory_engine(&self, buffers: &[&[u32]]) -> Vec<u32>;
+    fn operation_type(&self) -> Operation;
+}
